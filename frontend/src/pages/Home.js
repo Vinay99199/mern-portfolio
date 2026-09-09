3604d93239658b7255c480a04d6fdafabd6dd98b
Vinay <232792@kit.ac.in>
2026-09-09 17:18:33 +0530

    Remove production debug log

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -36,8 +36,6 @@ function Home() {
       try {
         const response = await axios.get(`${API}/api/projects`);
 
-        console.log("Projects API:", response.data);
-
         setProjects(response.data.data?.slice(0, 2) || []);
       } catch (error) {
         console.error("Projects API Error:", error);
